a7d9821730767687549908201d7cd380cc70de9c
Jan <104436608+janfvw@users.noreply.github.com>
2022-05-25 08:40:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ let playerentry ={
     punkte: 0,
 }
 let player1 = {
-    number: 0,
+    number: 1,
     track: "Normal",
-    punkte: 0,
+    punkte: 1:30,
     }
     let player2 = {
-    number: 0,
+    number: 2,
     track: "Normal",
-    punkte: 0,
+    punkte: 1:31,
     }
     let player3 = {
     number: 0,
