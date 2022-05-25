60fb9644bf7d22b3e4b54f01ba6c289423f80e86
Jan <104436608+janfvw@users.noreply.github.com>
2022-05-25 08:11:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let playerentry ={
     punkte: 0,
 }
 let player1 = {
-    number: 0,
-    track: "Urzeit",
-    punkte: 0,
+    number: 1,
+    track: "Normal",
+    punkte: 20,
     }
     let player2 = {
     number: 0,
