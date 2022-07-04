2cc686acc614e7a25ec23a11743d204045144863
expo22eis <108320232+expo22eis@users.noreply.github.com>
2022-07-04 15:40:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -178,9 +178,9 @@ let player1 = {
     punkte: 661,
     }
     let player29 = {
-    number: 2441,
+    number: 2377,
     track: "04.07.2022",
-    punkte: 643,
+    punkte: 644,
     }
     let player30 = {
     number: 2357,
